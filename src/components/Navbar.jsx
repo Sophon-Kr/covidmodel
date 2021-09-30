75c88a178dfb0f6fbee2ee557913b87c6454fec2
Sophon-Kr <77550438+Sophon-Kr@users.noreply.github.com>
2021-09-30 22:02:18 +0700

    innitial home structure

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,20 +1,108 @@
-import React from 'react'
-import { connect } from 'react-redux'
+import React, { useState } from "react";
+import { connect } from "react-redux";
+import Darklogo from "../Covid-logo-dark.png";
+import Lightlogo from "../Covid-logo-light.png";
+import AppBar from "@mui/material/AppBar";
+import Box from "@mui/material/Box";
+import Toolbar from "@mui/material/Toolbar";
+import Typography from "@mui/material/Typography";
+import Button from "@mui/material/Button";
+import IconButton from "@mui/material/IconButton";
+import MenuIcon from "@mui/icons-material/Menu";
+import Container from "@mui/material/Container";
+import SortIcon from "@mui/icons-material/Sort";
+import Collapse from "@mui/material/Collapse";
 
 export const Navbar = (props) => {
-    return (
-        <div>
-            Navbar
-        </div>
-    )
-}
+  const [isMobile, setIsMobile] = useState(false);
+  const handleMenuMobile = () => {
+    setIsMobile(!isMobile);
+  };
+  return (
+    <Box sx={{ flexGrow: 1 }} >
+      <AppBar position="static">
+        <Toolbar style={{ backgroundColor: "#2f5d8c", color: "#F9F9F9" }}>
+          <img
+            style={{ width: "45px", height: "45px", marginRight: 7 }}
+            src={Lightlogo}
+            alt="logo"
+          />
+          <Typography
+            variant="h6"
+            component="div"
+            sx={{ flexGrow: 1 }}
+            style={{ fontSize: 20 }}
+          >
+            COVID-19
+          </Typography>
+          <Box
+            sx={{
+              display: {
+                xs: "none",
+                sm: "none",
+                md: "block",
+                lg: "block",
+                xl: "block",
+              },
+            }}
+          >
+            <Button color="inherit">Home</Button>
+            <Button color="inherit">Model</Button>
+            <Button color="inherit">Data</Button>
+            <Button color="inherit">Contact</Button>
+          </Box>
+          <IconButton
+            sx={{
+              display: {
+                xs: "block",
+                sm: "block",
+                md: "none",
+                lg: "none",
+                xl: "none",
+              },
+              mr: 2,
+            }}
+            size="large"
+            edge="start"
+            color="inherit"
+            aria-label="menu"
+            onClick={handleMenuMobile}
+          >
+            {isMobile ? <SortIcon /> : <MenuIcon />}
+          </IconButton>
+        </Toolbar>
 
-const mapStateToProps = (state) => ({
-    
-})
+        <Collapse in={isMobile}>
+          <Container
+            maxWidth="xs"
+            style={{
+              backgroundColor: "#2f5d8c",
+              paddingLeft: 20,
+              paddingBottom: 10,
+            }}
+            sx={{
+              display: {
+                xs: "block",
+                sm: "blocknone",
+                md: "none",
+                lg: "none",
+                xl: "none",
+              },
+            }}
+          >
+            <Container style={{ padding: 10 }}>Home</Container>
+            <Container style={{ padding: 10 }}>Model</Container>
+            <Container style={{ padding: 10 }}>Data</Container>
+            <Container style={{ padding: 10 }}>Contact</Container>
+          </Container>
+        </Collapse>
+      </AppBar>
+    </Box>
+  );
+};
 
-const mapDispatchToProps = {
-    
-}
+const mapStateToProps = (state) => ({});
 
-export default connect(mapStateToProps, mapDispatchToProps)(Navbar)
+const mapDispatchToProps = {};
+
+export default connect(mapStateToProps, mapDispatchToProps)(Navbar);
--- src/page/Homepage.jsx
+++ src/page/Homepage.jsx
@@ -1,8 +1,73 @@
 import React from "react";
 import { connect } from "react-redux";
+import Container from "@mui/material/Container";
+import Paper from "@mui/material/Paper";
+import Typography from "@mui/material/Typography";
+import MainGraph from "../components/Graph/MainGraph";
+import Grid from "@mui/material/Grid";
+import { padding } from "@mui/system";
 
 export const Homepage = (props) => {
-  return <div>Homepage</div>;
+  return (
+    <Container
+      style={{ backgroundColor: "#F9F9F9", height: "100vh" }}
+      maxWidth="xl"
+    >
+      <Container maxWidth="xl" style={{ paddingTop: 30 }}>
+        <Grid container spacing={3}>
+          <Grid item xs={12} sm={12} md={6} lg={3} xl={3}>
+            <Paper
+              style={{
+                minHeight: 100,
+                padding: 30,
+                backgroundColor: "#D8D8D6",
+              }}
+            >
+              data1
+            </Paper>
+          </Grid>
+          <Grid item xs={12} sm={12} md={6} lg={3} xl={3}>
+            <Paper
+              style={{
+                minHeight: 100,
+                padding: 30,
+                backgroundColor: "#D8D8D6",
+              }}
+            >
+              data2
+            </Paper>
+          </Grid>
+          <Grid item xs={12} sm={12} md={6} lg={3} xl={3}>
+            <Paper
+              style={{
+                minHeight: 100,
+                padding: 30,
+                backgroundColor: "#D8D8D6",
+              }}
+            >
+              data3
+            </Paper>
+          </Grid>
+          <Grid item xs={12} sm={12} md={6} lg={3} xl={3}>
+            <Paper
+              style={{
+                minHeight: 100,
+                padding: 30,
+                backgroundColor: "#D8D8D6",
+              }}
+            >
+              data3
+            </Paper>
+          </Grid>
+        </Grid>
+      </Container>
+      <Container maxWidth="xl" style={{ paddingTop: 30 }}>
+        <Paper elevation={3} style={{ padding: 20 }}>
+          <MainGraph />
+        </Paper>
+      </Container>
+    </Container>
+  );
 };
 
 const mapStateToProps = (state) => ({});
