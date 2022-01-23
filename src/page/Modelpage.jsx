55cd6ec7ba8da405f2afb3d0fcddf862a1440f1c
Sophon-Kr <77550438+Sophon-Kr@users.noreply.github.com>
2022-01-23 21:46:03 +0700

    edit description node

--- src/page/Modelpage.jsx
+++ src/page/Modelpage.jsx
@@ -29,10 +29,9 @@ const modelDisplay = [
     name: "Susceptible Class : S",
     text: (
       <div>
-        &emsp; &emsp; &emsp; Lorem Ipsum is simply dummy text of the printing
-        and typesetting industry. Lorem Ipsum has been the industry's standard
-        dummy text ever since the 1500s, when an unknown printer took a galley
-        of type and scrambled it to make a type specimen book.
+        &emsp; &emsp; &emsp; คือ ผู้ที่สามารถรับเชื้อโรค หรือ
+        ผู้ที่ไม่มีความต้านทานต่อโรค
+        โดยของเราจะสื่อถึงประชากรของประเทศไทยที่ยังไม่ได้รับเชื้อโรคและยังไม่มีภูมิต้านทานต่อเชื้อตัวนี้โดยการได้รับวัคซีน
       </div>
     ),
     pic: <img src={S} alt="Susceptible" width="400" height="auto" />,
@@ -42,10 +41,13 @@ const modelDisplay = [
     name: "Vaccines I Class : V1",
     text: (
       <div>
-        &emsp; &emsp; &emsp; Lorem Ipsum is simply dummy text of the printing
-        and typesetting industry. Lorem Ipsum has been the industry's standard
-        dummy text ever since the 1500s, when an unknown printer took a galley
-        of type and scrambled it to make a type specimen book.
+        &emsp; &emsp; &emsp; คือ
+        ประชากรในประเทศไทยที่ได้รับภูมิต้านทานจากการฉีดวัคซีนในโดสแรก
+        ในประเทศไทยได้เริ่มมีการแจกจ่ายวัคซีนให้แก่ประชากรในประเทศไทยเป็นครั้งแรก
+        ในวันที่ 28 กุมภาพันธ์ 2564 แต่ในการทำของเราจะใช้ข้อมูลเริ่มต้นในวันที่
+        01 มีนาคม 2564 โดยฉีดวัคซีนเพื่อป้องกันโรคโควิด-19 สามารถลดการแพร่ระบาด
+        ลดความรุนแรงของอาการป่วย และลดการเสียชีวิตได้
+        ประกาศจำนวนจากทางกระทรวงสาธารณสุข
       </div>
     ),
     pic: <img src={V1} alt="Vaccines1" width="400" height="auto" />,
@@ -55,10 +57,11 @@ const modelDisplay = [
     name: "Vaccines I Class : V2",
     text: (
       <div>
-        &emsp; &emsp; &emsp; Lorem Ipsum is simply dummy text of the printing
-        and typesetting industry. Lorem Ipsum has been the industry's standard
-        dummy text ever since the 1500s, when an unknown printer took a galley
-        of type and scrambled it to make a type specimen book.
+        &emsp; &emsp; &emsp;คือ
+        ประชากรในประเทศไทยที่ได้รับภูมิต้านทานจากการฉีดวัคซีนในโดสที่สอง
+        เพื่อให้มีภูมิต้านทานที่เพิ่มขึ้น
+        โดยการฉีดวัคซีนครบ2เข็มจะถือว่าเป็นการได้รับวัคซีนครบโดสแล้ว
+        ประกาศจำนวนจากทางกระทรวงสาธารณสุข
       </div>
     ),
     pic: <img src={V2} alt="Vaccines2" width="400" height="auto" />,
@@ -68,10 +71,13 @@ const modelDisplay = [
     name: "Maintenance Shot Class : M",
     text: (
       <div>
-        &emsp; &emsp; &emsp; Lorem Ipsum is simply dummy text of the printing
-        and typesetting industry. Lorem Ipsum has been the industry's standard
-        dummy text ever since the 1500s, when an unknown printer took a galley
-        of type and scrambled it to make a type specimen book.
+        &emsp; &emsp; &emsp; คือ
+        ประชากรในประเทศไทยที่ได้รับภูมิต้านทานจากการฉีดวัคซีนเข็มกระตุ้น
+        โดยต้องมีการฉีดเข็มกระตุ้นเนื่องจาก เมื่อระยะเวลาผ่านไป
+        ระดับภูมิคุ้มจากวัคซีนชุดแรกที่ได้รับจะค่อยๆ ลดลง
+        ทำให้ประสิทธิภาพในการป้องกันลดลง
+        จึงต้องมีการรับวัคซีนเข็มกระตุ้นเพื่อกระตุ้นประสิทธิภาพของวัคซีน
+        ประกาศจำนวนจากทางกระทรวงสาธารณสุข
       </div>
     ),
     pic: <img src={M} alt="Maintenance Shot" width="400" height="auto" />,
@@ -81,10 +87,8 @@ const modelDisplay = [
     name: "Infection I Class : I ",
     text: (
       <div>
-        &emsp; &emsp; &emsp; Lorem Ipsum is simply dummy text of the printing
-        and typesetting industry. Lorem Ipsum has been the industry's standard
-        dummy text ever since the 1500s, when an unknown printer took a galley
-        of type and scrambled it to make a type specimen book.
+        &emsp; &emsp; &emsp; คือ
+        ประชากรที่ได้รับการยืนยันว่าติดเชื้อโรคแล้วในประเทศไทยประกาศจำนวนจากทางกระทรวงสาธารณสุข
       </div>
     ),
     pic: <img src={I} alt="Infection" width="400" height="auto" />,
