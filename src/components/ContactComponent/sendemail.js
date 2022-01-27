b8d5ffa862841e7647c7e205dc01aa8e5664905a
Sophon-Kr <77550438+Sophon-Kr@users.noreply.github.com>
2022-01-27 23:49:42 +0700

    add detail in nodemailer

--- src/components/ContactComponent/sendemail.js
+++ src/components/ContactComponent/sendemail.js
@@ -1,4 +1,5 @@
 var nodemailer = require("nodemailer");
+
 var transporter = nodemailer.createTransport({
   service: "gmail",
   auth: {
@@ -7,17 +8,28 @@ var transporter = nodemailer.createTransport({
   },
 });
 
+var subjectData = "Sending Email using Node.js";
+var sendTo = ["sophonkripinit@gmail.com"];
+var sendFrom = "sender@server.com";
+var sendText =
+  // eslint-disable-next-line no-multi-str
+  "Lorem Ipsum is simply dummy text of the printing \
+and typesetting industry. Lorem Ipsum has been the industry's standard \
+dummy text ever since the 1500s, when an unknown printer took a galley \
+of type and scrambled it to make a type specimen book.";
+
 var mailOptions = {
-  from: "testmyemaol@gmail.com",
-  to: "sophonkripinit@gmail.com",
-  subject: "Sending Email using Node.js",
-  text: "That was easy!",
+  from: sendFrom,
+  to: sendTo,
+  subject: subjectData,
+  text: sendText,
 };
 
 const sendMail = transporter.sendMail(mailOptions, function (error, info) {
   if (error) {
     console.log(error);
   } else {
-    console.log("Email sent: " + info.response);
+    console.log("Email sent : info.response:: " + info.response);
+    // console.log("sendMail: " + sendMail);
   }
 });
