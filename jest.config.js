fbe4c046ebd6412270191dab4ec7fe85a8737d21
Sophon-Kr <77550438+Sophon-Kr@users.noreply.github.com>
2022-04-07 01:04:23 +0700

    fix error img test

--- jest.config.js
+++ jest.config.js
@@ -1,3 +1,7 @@
 module.exports = {
   setupFilesAfterEnv: ["./test/setupTests.js"],
+  moduleNameMapper: {
+    "\\.(css)$": "identity-obj-proxy",
+    "\\.(jpg|jpeg|png)$": "identity-obj-proxy",
+  },
 };
--- test/Add.test.js
+++ test/Add.test.js
@@ -1,9 +1,31 @@
 import Add from "../src/components/Add";
+import Modelpage from "../src/page/Modelpage";
+
+import configureStore from "../src/middleware/store";
+
+const setUp = (initialState = {}) => {
+  const store = configureStore();
+  // eslint-disable-next-line no-undef
+  const wrapper = shallow(<Modelpage store={store} />)
+    .childAt(0)
+    .dive();
+  return wrapper;
+};
+setUp();
 
 describe("<Add /> rendering", () => {
   it("should render one <h1>", () => {
     // eslint-disable-next-line no-undef
     let wrapper = shallow(<Add />);
+    expect(wrapper.children("h1")).toHaveLength(1);
+  });
+});
+
+describe("<Modelpage /> rendering", () => {
+  it("should render one <h1>", () => {
+    const store = configureStore();
+    // eslint-disable-next-line no-undef
+    let wrapper = shallow(<Modelpage store={store} />);
     expect(wrapper.children("h1")).toHaveLength(1);
   });
 });
